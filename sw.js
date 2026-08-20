b595f10137a2bd9790ce2adf923003dda9db57cf
Tam quốc việt nam <tuankjet2401@gmail.com>
2026-08-20 18:22:55 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+
+const CACHE="dai-viet-3-3-v1";
+const FILES=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
